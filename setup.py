202ecec1ea2478022ce7302fbbdb46eb96eb8b44
Sakib Ahmed <66124765+Sakib323@users.noreply.github.com>
2025-05-30 00:31:54 +0600

    added adaln conditioning and made changes on arch to convert the ternery lm model into ternery dit model

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     author_email="sakibahmed2018go@gmail.com",
     url="https://github.com/Sakib323/AI-Game-Engine",
     packages=find_packages(include=["mmfreelm", "mmfreelm.*"]),
-    py_modules=["noise_latent", "diffusion_model", "patch_embedding","modeling_hgrn_bit"],
+    py_modules=["noise_latent", "diffusion_model", "patch_embedding"],
     install_requires=[
         "torch>=1.9.0",
         "numpy>=1.19.0",
