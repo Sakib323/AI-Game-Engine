dbc20ce538f2d3f541228a2788dc0fa208aa6081
Sakib Ahmed <66124765+Sakib323@users.noreply.github.com>
2025-09-25 20:13:18 +0600

    texture_dit

--- mmfreelm/models/__init__.py
+++ mmfreelm/models/__init__.py
@@ -3,9 +3,11 @@
 from mmfreelm.models.hgrn_bit import HGRNBitConfig, HGRNBitForCausalLM, HGRNBitModel
 from mmfreelm.models.hgrn_bit.ternary_dit import TimestepEmbedder, DiTBlock, TextEmbedder, FinalLayer, DiT_models
 from mmfreelm.models.hgrn_bit.mesh_dit import TimestepEmbedder, TextEmbedder, FinalLayer, MeshDiT_models
+from mmfreelm.models.hgrn_bit.texture_dit import TernaryMVAdapter
+
 
 
 __all__ = [
-    'HGRNBitConfig', 'HGRNBitForCausalLM', 'HGRNBitModel','TimestepEmbedder','DiTBlock','FinalLayer','TextEmbedder','FinalLayerSecond','DiT_models','MeshDiT_models'
+    'HGRNBitConfig', 'HGRNBitForCausalLM', 'HGRNBitModel','TimestepEmbedder','DiTBlock','FinalLayer','TextEmbedder','FinalLayerSecond','DiT_models','MeshDiT_models','TernaryMVAdapter'
 
 ]
--- mmfreelm/models/hgrn_bit/__init__.py
+++ mmfreelm/models/hgrn_bit/__init__.py
@@ -7,6 +7,8 @@ from mmfreelm.models.hgrn_bit.modeling_hgrn_bit import HGRNBitForCausalLM, HGRNB
 from mmfreelm.models.hgrn_bit.rotary_embedding import RotaryEmbedding, apply_rotary_pos_emb, rotate_half
 from mmfreelm.models.hgrn_bit.ternary_dit import TimestepEmbedder, DiTBlock, TextEmbedder, FinalLayer, DiT_models
 from mmfreelm.models.hgrn_bit.mesh_dit import MeshDiT_models
+from mmfreelm.models.hgrn_bit.texture_dit import TernaryMVAdapter
+
 
 AutoConfig.register(HGRNBitConfig.model_type, HGRNBitConfig)
 AutoModel.register(HGRNBitConfig, HGRNBitModel)
@@ -26,4 +28,5 @@ __all__ = [
     'FinalLayerSecond',
     'DiT_models',
     'MeshDiT_models',
+    'TernaryMVAdapter'
 ]
