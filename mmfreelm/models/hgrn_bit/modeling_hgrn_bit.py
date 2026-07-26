f4c11ec4c3d8183113eab887ee6fc8b34469a35a
Sakib Ahmed <66124765+Sakib323@users.noreply.github.com>
2026-07-26 13:32:45 +0100

    Update __init__ files and remove unused imports

--- mmfreelm/models/hgrn_bit/modeling_hgrn_bit.py
+++ mmfreelm/models/hgrn_bit/modeling_hgrn_bit.py
@@ -530,8 +530,8 @@ class HGRNBitForCausalLM(HGRNBitPreTrainedModel, GenerationMixin):
     HGRN-Bit causal language model.
     """
 
-    _tied_weights_keys = {"lm_head.weight": "model.embeddings.weight"}
-
+    #_tied_weights_keys = {"lm_head.weight": "model.embeddings.weight"}
+    _tied_weights_keys = ["lm_head.embedding.weight"]
     def __init__(
         self,
         config: HGRNBitConfig,
